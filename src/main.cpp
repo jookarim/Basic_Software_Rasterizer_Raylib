adef6b44007ecb60c269a3957032aa07bcacf2e0
Youssef karim <joo.karim2013@gmail.com>
2026-03-21 19:27:47 +0200

    Add toggle between naive vs optimized renderer

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@
 #include <string>
 #include <sstream>
 #include <iomanip>
+#include <rlImGui.h>
+#include <imgui.h>
 
-#define WINDOW_WIDTH 800
-#define WINDOW_HEIGHT 600
+
+#define WINDOW_WIDTH 900
+#define WINDOW_HEIGHT 700
 
 Vector2 operator-(const Vector2& a, const Vector2& b)
 {
@@ -46,7 +49,7 @@ void rotateVector(Vector2& vec, float angle, const Vector2& center)
     vec.y += center.y;
 }
 
-void drawTriangle(Vector2 v0, Vector2 v1, Vector2 v2, Color c0, Color c1, Color c2)
+void drawTriangleNew(Vector2 v0, Vector2 v1, Vector2 v2, Color c0, Color c1, Color c2)
 {
     int minX = (int)std::floor(std::min({ v0.x, v1.x, v2.x }));
     int minY = (int)std::floor(std::min({ v0.y, v1.y, v2.y }));
@@ -96,7 +99,7 @@ void drawTriangle(Vector2 v0, Vector2 v1, Vector2 v2, Color c0, Color c1, Color 
                 int g = (int)(alpha * c0.g + beta * c1.g + gamma * c2.g);
                 int b = (int)(alpha * c0.b + beta * c1.b + gamma * c2.b);
 
-                Color color = { r, g, b, 255 };
+                Color color = { (unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)255 };
 
                 DrawPixel(x, y, color);
             }
@@ -112,6 +115,51 @@ void drawTriangle(Vector2 v0, Vector2 v1, Vector2 v2, Color c0, Color c1, Color 
     }
 }
 
+void drawTriangleOld(Vector2 v0, Vector2 v1, Vector2 v2, Color c0, Color c1, Color c2)
+{
+    int minX = (int)std::floor(std::min({ v0.x, v1.x, v2.x }));
+    int minY = (int)std::floor(std::min({ v0.y, v1.y, v2.y }));
+
+    int maxX = (int)std::ceil(std::max({ v0.x, v1.x, v2.x }));
+    int maxY = (int)std::ceil(std::max({ v0.y, v1.y, v2.y }));
+
+    float area = edge(v0, v1, v2);
+
+
+    for (int y = minY; y <= maxY; y++)
+    {
+        for (int x = minX; x <= maxX; x++)
+        {
+            Vector2 p = { (float)x + 0.5f, (float)y + 0.5f };
+
+            float w0 = edge(v1, v2, p);
+            float w1 = edge(v2, v0, p);
+            float w2 = edge(v0, v1, p);
+
+            bool inside = true;
+
+            if (w0 < 0.f || (w0 == 0.f && !top_left_edge(v1, v2))) inside = false;
+            if (w1 < 0.f || (w1 == 0.f && !top_left_edge(v2, v0))) inside = false;
+            if (w2 < 0.f || (w2 == 0.f && !top_left_edge(v0, v1))) inside = false;
+
+            if (inside)
+            {
+                float alpha = w0 / area;
+                float beta = w1 / area;
+                float gamma = w2 / area;
+
+                int r = (int)(alpha * c0.r + beta * c1.r + gamma * c2.r);
+                int g = (int)(alpha * c0.g + beta * c1.g + gamma * c2.g);
+                int b = (int)(alpha * c0.b + beta * c1.b + gamma * c2.b);
+
+                Color color = { (unsigned char)r, (unsigned char)g, (unsigned char)b, (unsigned char)255 };
+
+                DrawPixel(x, y, color);
+            }
+        }
+    }
+}
+
 Vector2 getCenter(Vector2 triangle[3])
 {
     float centerX = (triangle[0].x + triangle[1].x + triangle[2].x) / 3.f;
@@ -121,19 +169,19 @@ Vector2 getCenter(Vector2 triangle[3])
 
 int main()
 {
-    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Full Subpixel Rasterizer");
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Software Rasterizer");
     SetTargetFPS(60);
 
     Vector2 tri1[] = {
-        { 200, 120 },
-        { 350, 500 },
-        { 80, 480 }
+        { 250, 120 },
+        { 400, 500 },
+        { 130, 480 }
     };
 
     Vector2 tri2[] = {
-        { 360, 140 },
-        { 550, 520 },
-        { 250, 500 }
+        { 410, 140 },
+        { 600, 520 },
+        { 300, 500 }
     };
 
     Color tri1Colors[] = {
@@ -147,6 +195,10 @@ int main()
         {25, 87, 122, 255},
         {0, 0, 255, 255}
     };
+    
+    rlImGuiSetup(true);
+
+    bool toggle = false;
 
     while (!WindowShouldClose())
     {
@@ -167,8 +219,27 @@ int main()
         rotateVector(tri1[1], angle, center);
         rotateVector(tri1[2], angle, center);
 
-        drawTriangle(tri1[0], tri1[1], tri1[2], tri1Colors[0], tri1Colors[1], tri1Colors[2]);
-        drawTriangle(tri2[0], tri2[1], tri2[2], tri2Colors[0], tri2Colors[1], tri2Colors[2]);
+        rlImGuiBegin();
+
+        ImGui::Begin("Toggle Performance");
+
+        ImGui::Checkbox("Optimized Rasterization: ", &toggle);
+
+        if (toggle)
+        {
+            drawTriangleNew(tri1[0], tri1[1], tri1[2], tri1Colors[0], tri1Colors[1], tri1Colors[2]);
+            drawTriangleNew(tri2[0], tri2[1], tri2[2], tri2Colors[0], tri2Colors[1], tri2Colors[2]);
+        }
+       
+        else
+        {
+            drawTriangleOld(tri1[0], tri1[1], tri1[2], tri1Colors[0], tri1Colors[1], tri1Colors[2]);
+            drawTriangleOld(tri2[0], tri2[1], tri2[2], tri2Colors[0], tri2Colors[1], tri2Colors[2]);
+        }
+
+        ImGui::End();
+        rlImGuiEnd();
+        
 
         EndDrawing();
     }
